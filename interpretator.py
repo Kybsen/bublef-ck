255db4812296c127d41953fd6021fab85aa9e536
Kybsen <126771763+Kybsen@users.noreply.github.com>
2024-02-21 16:04:05 +0300

    Update interpretator.py
    We need less letters, let's make it smaller!

--- interpretator.py
+++ interpretator.py
@@ -1,41 +1,22 @@
 #!/bin/python3
 
+
 memory = [0, ]  
-
 cursor = 0
-
 def main(line):
     global cursor, memory
-    
     for i in line:
-        if i == "&":
-            memory.append(0)
-
-        if i == "!":
-            cursor += 1
-
-        if i == "%":
-            cursor -= 1
-
-        if i == "@":
-            memory[cursor] += 1
-
-        if i == "$":
-            memory[cursor] -= 1
-
-
+        if i == "&": memory.append(0)
+        if i == "!": cursor += 1
+        if i == "%": cursor -= 1
+        if i == "@": memory[cursor] += 1
+        if i == "$": memory[cursor] -= 1
+    print(*memory, f"; cursor : {cursor}") 
 if __name__ == "__main__":
     print("""\nA very useless programming language!\n""")
     line = ""
     while line != ".stop":
         line = input('>>>')
-        if line == ".clear":
-            memory = [0, ]
-            continue
+        if line == ".clear": memory = [0, ]
+        else: main(line)          
         
-        else:
-            main(line)
-            print(*memory)
-            print(cursor, ': cursor')        
-        
-
