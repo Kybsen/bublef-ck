3c196e68b01555c84279419c00069c9215f8c616
Kybsen <126771763+Kybsen@users.noreply.github.com>
2024-02-21 16:07:04 +0300

    Update interpretator.py
    Pre-final version

--- interpretator.py
+++ interpretator.py
@@ -1,22 +1,17 @@
 #!/bin/python3
-
-
 memory = [0, ]  
 cursor = 0
-def main(line):
-    global cursor, memory
-    for i in line:
-        if i == "&": memory.append(0)
-        if i == "!": cursor += 1
-        if i == "%": cursor -= 1
-        if i == "@": memory[cursor] += 1
-        if i == "$": memory[cursor] -= 1
+print("""\nBuBleFuck\n""")
+line = ""
+while line != ".stop":
+    line = input('>>>')
+    if line == ".clear": memory = [0, ]
+    else: 
+        for i in line:
+            if i == "&": memory.append(0)
+            if i == "!": cursor += 1
+            if i == "%": cursor -= 1
+            if i == "@": memory[cursor] += 1
+            if i == "$": memory[cursor] -= 1
+
     print(*memory, f"; cursor : {cursor}") 
-if __name__ == "__main__":
-    print("""\nA very useless programming language!\n""")
-    line = ""
-    while line != ".stop":
-        line = input('>>>')
-        if line == ".clear": memory = [0, ]
-        else: main(line)          
-        
